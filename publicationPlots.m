0d1d83292aa8c5a9024bcf01cbc1964f1feea6bc
mcvinding <mikkel.vinding@gmail.com>
2018-11-02 11:46:29 +0100

    Changes to figures

--- publicationPlots.m
+++ publicationPlots.m
@@ -202,17 +202,18 @@ export_fig('beta_evo.png', '-r500', '-p0.05', '-CMYK', '-png', '-transparent')
 %% Single subject plots
 cd(dirs.export);
 
-fig = figure('rend','painters','pos',[10 10 750 1000]); hold on
+fig = figure('rend','painters','pos',[10 10 750 1250]); hold on
 set(fig,'PaperPosition', [0 0 4 2], 'color','w');
 for ss = 1:length(PD_beta1bs)
     subplot(4,3,ss); hold on
     axis([-.5, 2.5, -0.6, 0.4])
-    line([0 0],[-0.6, 0.5],'color',[0.5 .5 .5],'LineStyle',':','LineWidth',2)
-    line([-.5, 2.5],[0 0],'color',[0.5 .5 .5],'LineStyle',':','LineWidth',2)    
-    ps1 = plot(BdataPD1.time,BtracePD1,'k-','LineWidth',2);
-    ps1.Color(4) = 0.75;
-    ps2 = plot(BdataPD1.time,BtracePD2,'k:','LineWidth',2);
-    ps2.Color(4) = 0.75;
+    line([0 0],[-0.6, 0.5],'color',[0.1 0.1 0.1],'LineStyle',':','LineWidth',1)
+    line([-.5, 2.5],[0 0],'color',[0.1 0.1 0.1],'LineStyle',':','LineWidth',1)
+    plot(BdataCtrl1.time,BtraceCtrl1,'-','LineWidth',2, 'color',[.5,.5,.5]);
+    plot(BdataCtrl2.time,BtraceCtrl2,':','LineWidth',2, 'color',[.5,.5,.5]);
+    plot(BdataCtrl1.time,BtracePD1,'-','LineWidth',2, 'color',[.1,.1,.1]);
+    plot(BdataCtrl2.time,BtracePD2,'k:','LineWidth',2, 'color',[.1,.1,.1]);
+    
     plot(timeDim,BtracePD1sub{ss},'b-','LineWidth',2);
     plot(timeDim,BtracePD2sub{ss},'r-','LineWidth',2);
     if ismember(ss, 1:3:length(PD_beta1bs))
@@ -224,19 +225,20 @@ for ss = 1:length(PD_beta1bs)
     title(['PD ',num2str(ss)],'fontsize',12);
     set(gca, 'LineWidth', 2,'fontweight','bold');
 end
-export_fig('beta_evo_subPD2.png', '-r500', '-p0.05', '-CMYK', '-png', '-transparent')
+export_fig('beta_evo_subPD2.png', '-r500', '-q100','-p0.05', '-CMYK', '-png', '-transparent')
 
-fig = figure('rend','painters','pos',[10 10 1000 1000]); hold on
+fig = figure('rend','painters','pos',[10 10 1000 1250]); hold on
 set(fig,'PaperPosition', [0 0 4 2], 'color','w');
 for ss = 1:length(ctrl_beta1bs)
     subplot(4,4,ss); hold on
     axis([-.5, 2.5, -0.6, 0.4])
-    line([0 0],[-0.6, 0.5],'color',[0.5 .5 .5],'LineStyle',':','LineWidth',2)
-    line([-.5, 2.5],[0 0],'color',[0.5 .5 .5],'LineStyle',':','LineWidth',2)
-    ps1 = plot(BdataCtrl1.time,BtraceCtrl1,'k-','LineWidth',2);
-    ps1.Color(4) = 0.75;
-    ps2 = plot(BdataCtrl2.time,BtraceCtrl2,'k:','LineWidth',2);
-    ps2.Color(4) = 0.75;
+    line([0 0],[-0.6, 0.5],'color',[0.1 0.1 0.1],'LineStyle',':','LineWidth',1)
+    line([-.5, 2.5],[0 0],'color',[0.1 0.1 0.1],'LineStyle',':','LineWidth',1)
+    plot(BdataCtrl1.time,BtraceCtrl1,'-','LineWidth',2, 'color',[.5,.5,.5]);
+    plot(BdataCtrl2.time,BtraceCtrl2,':','LineWidth',2, 'color',[.5,.5,.5]);
+    plot(BdataCtrl1.time,BtracePD1,'-','LineWidth',2, 'color',[.1,.1,.1]);
+    plot(BdataCtrl2.time,BtracePD2,'k:','LineWidth',2, 'color',[.1,.1,.1]);
+    
     plot(timeDim,BtraceCtrl1sub{ss},'b-','LineWidth',2);
     plot(timeDim,BtraceCtrl2sub{ss},'r-','LineWidth',2);
     if ismember(ss, 1:4:length(ctrl_beta1bs))
@@ -248,7 +250,25 @@ for ss = 1:length(ctrl_beta1bs)
     title(['HC ',num2str(ss)],'fontsize',12);
     set(gca, 'LineWidth', 2,'fontweight','bold');
 end
-export_fig('beta_evo_subHC2.png', '-r500', '-p0.05', '-CMYK', '-png', '-transparent')
+export_fig('beta_evo_subHC2.png', '-r500', '-q100','-p0.05', '-CMYK', '-png', '-transparent')
+
+% Extra plot
+fig = figure('rend','painters','pos',[10 10 600 400]); hold on
+set(fig,'PaperPosition', [0 0 4 2], 'color','w');
+patch([clustStart clustEnd clustEnd clustStart],[-0.6 -0.6 0.4 0.4],[.1,.1,.1],'FaceAlpha',0.1,'EdgeColor','none')
+p1=plot(BdataCtrl1.time,BtraceCtrl1,'-','LineWidth',3, 'color',[.5,.5,.5]);
+p2=plot(BdataCtrl2.time,BtraceCtrl2,':','LineWidth',3, 'color',[.5,.5,.5]);
+p3=plot(BdataCtrl1.time,BtracePD1,'-','LineWidth',3, 'color',[.1,.1,.1]);
+p4=plot(BdataCtrl2.time,BtracePD2,'k:','LineWidth',3, 'color',[.1,.1,.1]);    
+axis([-.5, 2.5, -0.4, 0.2])
+line([0 0],[-0.6, 0.4],'color',[0.5 .5 .5],'LineStyle','--','LineWidth',2)
+xlabel('Time (s)','fontsize',16);
+title('Beta-band spectral evolution','fontsize',16);
+set(gca, 'LineWidth', 2,'fontweight','bold','fontsize',14);
+legend([p1,p2,p3,p4],{'PD OFF','PD ON','HC Session1','HC Session2'},'Location','SouthEast')
+legend BOXOFF
+export_fig('beta_evoBW.png', '-r500', '-p0.05', '-CMYK', '-png', '-transparent')
+
 
 %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Define bands
